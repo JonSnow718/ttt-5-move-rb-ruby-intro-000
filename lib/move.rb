aae185786062a83e4085e94094a0c828086f4a15
Jason Calvo <jason.calvo7@gmail.com>
2020-04-01 23:46:05 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -17,11 +17,3 @@ end
 def move(board,index, player)
   board[index] = player 
 end
-
-def position_taken?(board, index)
-   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-      return false 
-   else
-      return true
-   end
-end
